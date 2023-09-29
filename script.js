6ac8f860853f4ebdea86fc4cae3b7ca220c0e45f
777dan <danya.nikitin7@gmail.com>
2023-09-29 15:55:43 +0300

    changed functions

--- script.js
+++ script.js
@@ -5,35 +5,37 @@ const questions = [
     "What should I take to the pool?"
 ];
 const answers = ["skates", "apples", "textbooks", "towel"];
+document.write(randomArrayValue(answers, 4));
+// document.write(generateResult(questions, answers));
 
-document.write(generateResult(questions, answers));
-
-function generateResult(questions, answers) {
-    let str = "";
-    for (let i = 0; i < questions.length; i++) {
-        str += `${randomArrayValue(questions)} - `;
-        let rand = getRandomInt(4);
-        for (let j = 0; j <= rand; j++) {
-            str += `${randomArrayValue(answers)}`;
-            if (rand !== j) {
-                str += ", ";
-            } else {
-                str += "<br>";
-            }
-        }
-    }
-    return str;
-}
+// function generateResult(questions, answers) {
+//     let str = "";
+//     for (let i = 0; i < questions.length; i++) {
+//         str += `${randomArrayValue(questions)} - `;
+//         let rand = getRandomInt(4);
+//         for (let j = 0; j <= rand; j++) {
+//             str += `${randomArrayValue(answers)}`;
+//             if (rand !== j) {
+//                 str += ", ";
+//             } else {
+//                 str += "<br>";
+//             }
+//         }
+//     }
+//     return str;
+// }
 
 function getRandomInt(n) {
     return Math.floor(Math.random() * n);
 }
 
-function randomArrayValue(array) {
-    let str;
+function randomArrayValue(array, num = 1) {
+    let str = "";
     let rand = 0;
-    rand = getRandomInt(array.length);
-    str = array[rand];
+    for (let i = 0; i < num; i++) {
+        rand = getRandomInt(array.length);
+        str += `${array[rand]} `;
+    }
 
     return str;
 }
